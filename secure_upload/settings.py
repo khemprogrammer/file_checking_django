9f7799aab44e051ea8d51dde8baa819e4a979a60
khem Bahadur Lodh <55244922+khemprogrammer@users.noreply.github.com>
2026-03-23 03:36:27 +0545

    Update settings.py
    Fix static files config for Django 5 and Render

--- secure_upload/settings.py
+++ secure_upload/settings.py
@@ -69,7 +69,14 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
